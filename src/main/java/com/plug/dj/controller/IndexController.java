14722d2a7c8d5f0a7ea94ad3cc38d0ae1d9c983a
alice965 <alice965@naver.com>
2017-10-18 09:39:00 +0900

    10/18 설정

---
+++ src/main/java/com/plug/dj/controller/IndexController.java
@@ -0,0 +1,28 @@
+package com.plug.dj.controller;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class IndexController {
+
+	@RequestMapping({ "/", "/index" })
+	public ModelAndView rootHandle() {
+		ModelAndView mav = new ModelAndView("t_expr");
+			mav.addObject("section", "index");
+		return mav;
+	}
+} 
