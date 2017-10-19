990b2fbf929c73652259a600b72626051418bc5d
victor514 <victor514@naver.com>
2017-10-19 11:33:40 +0900

    임시 커밋

---
+++ src/main/webapp/js/default.js
@@ -0,0 +1,14 @@
+/* bootstrap theme  https://bootsnipp.com/snippets/Bqy7E  */
+
+// toggle class scroll 
+$(window).scroll(function() {
+    if($(this).scrollTop() > 50)
+    {
+        $('.navbar-trans').addClass('afterscroll');
+    } else
+    {
+        $('.navbar-trans').removeClass('afterscroll');
+    }  
+
+});
+  
